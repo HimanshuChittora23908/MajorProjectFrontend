b9a4c14c100bdab85083f078bfa63404341d1123
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2023-04-27 19:16:58 +0530

    csv upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [file, setFile] = useState(null);
+
+  console.log(file);
+
+  // Send uploaded csv file to server
+  const handleFileUpload = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    axios
+      .post("http://127.0.0.1:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="flex flex-col gap-y-2 items-center justify-center h-screen">
+      <p>Please select a csv file to upload</p>
+      <input
+        type="file"
+        accept=".csv"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleFileUpload}
+      >
+        Upload
+      </button>
+    </h1>
   );
 }
-
-export default App;
